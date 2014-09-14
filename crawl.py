5026bd37456f05dc210955a2e6911f11cc05acbd
root <root@localhost.(none)>
2014-09-14 23:28:14 +0200

    optimizations....

--- crawl.py
+++ crawl.py
@@ -5,22 +5,22 @@ from bs4 import BeautifulSoup
 def trade_spider(max_pages):
     page = 1
     while page <= max_pages:
-        url = "http://berufsstart.monster.de/searchresults/home.aspx?q=Energiemanagement&pg=" + str(page)
+        url = ("http://jobsuche.monster.de/Jobs/"
+               "?q=Energiemanager&pg=" + str(page) + "&cy=de")
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text)
-        for link in soup.findAll('a', {'target': '_blank'}):
+        for link in soup.findAll('a', {'class': 'slJobTitle fnt11'}):
             Stelle = link.string
             Link = link.get('href')
-        for item_name in soup.findAll('div', {'class': 'fnt16'}):
-            Firma = item_name.string
+        # for image in soup.findAll('img', {'class': 'trovixCompanyLogo'}):
+        #     Firma = image.get('alt', '')
 
             print("------")
             print(Stelle)
-            print(Firma)
+            # print(Firma)
             print(Link)
-
 
         page += 1
 
-trade_spider(2)
+trade_spider(1)
