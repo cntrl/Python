659c0b3573e931f9af8839d4b401f0f006e2b3e7
root <root@ip-172-31-43-254.eu-west-1.compute.internal>
2014-09-15 15:56:23 +0000

    crawl subsites

--- crawl.py
+++ crawl.py
@@ -2,6 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def hyperlink_crawler(item_url):
+    sourceCode = requests.get(item_url)
+    sourceText = sourceCode.text
+    soup = BeautifulSoup(sourceText)
+    for header in soup.findAll('title'):
+        global jobtitel, href
+        jobtitel = header.string
+        href = ""
+
+    for link in soup.findAll('a', {'data-track': 'ATB-AllJobs'}):
+        href = link.get('href')
+        href = href[47:len(href)]
+        href = href.replace('+', ' ')
+        print href
+
+    heading = soup.findAll('h1')
+    print(heading)
+
+
 def trade_spider(max_pages):
     page = 1
     while page <= max_pages:
@@ -12,16 +31,15 @@ def trade_spider(max_pages):
         soup = BeautifulSoup(plain_text)
         links = soup.findAll('a', {'class': lambda L: L and L.startswith('slJobTitle')})
         for link in links:
-            Stelle = link.string
             Link = link.get('href')
-        # for image in soup.findAll('img', {'class': 'trovixCompanyLogo'}):
-        #     Firma = image.get('alt', '')
-
             print("------")
-            print(Stelle)
             # print(Firma)
             print(Link)
+            hyperlink_crawler(Link)
+            page += 1
+        # for image in soup.findAll('img', {'class': 'trovixCompanyLogo'}):
+        #     Firma j image.get('alt', '')
 
-        page += 1
+
 
 trade_spider(2)
