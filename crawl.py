3d1aad1af85740d5803091d0d01d6187bf62c751
root <root@ip-172-31-43-254.eu-west-1.compute.internal>
2014-09-18 10:46:59 +0000

    Variablen verändert

--- crawl.py
+++ crawl.py
@@ -2,10 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_subsites(item_url):
-    sourceCode = requests.get(item_url)
-    sourceText = sourceCode.text
-    soup = BeautifulSoup(sourceText)
+def mainCrawler(max_pages):
+    page = 1
+    while page <= max_pages:
+        url = ("http://jobsuche.monster.de/Jobs/"
+               "?q=Energiemanager&pg=" + str(page) + "&cy=de")
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text)
+        links = soup.findAll('a', {'class': lambda L: L and L.startswith
+                                   ('slJobTitle')})
+        for link in links:
+            Link = link.get('href')
+            print("------")
+            print(Link)
+            getHyperlinks(Link)
+        for image in soup.findAll('img', {'class': 'trovixCompanyLogo'}):
+            Firma = image.get('alt', '')
+            print(image)
+            print(Firma)
+
+        page += 1
+
+
+def getHyperlinks(item_url):
+    Quellcode = requests.get(item_url)
+    Quelltext = Quellcode.text
+    soup = BeautifulSoup(Quelltext)
     for header in soup.findAll('title'):
         global jobtitel, href
         jobtitel = header.string
@@ -20,28 +43,4 @@ def get_subsites(item_url):
     heading = soup.findAll('h1')
     print(heading)
 
-
-def main_crawler(max_pages):
-    page = 1
-    while page <= max_pages:
-        url = ("http://jobsuche.monster.de/Jobs/"
-               "?q=Energiemanager&pg=" + str(page) + "&cy=de")
-        source_code = requests.get(url)
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text)
-        links = soup.findAll('a', {'class': lambda L: L and L.startswith
-                                   ('slJobTitle')})
-        for link in links:
-            Link = link.get('href')
-            print("------")
-            print(Link)
-            get_subsites(Link)
-        for image in soup.findAll('img', {'class': 'trovixCompanyLogo'}):
-            Firma = image.get('alt', '')
-            print(image)
-            print(Firma)
-
-        page += 1
-
-
-main_crawler(1)
+mainCrawler(1)
