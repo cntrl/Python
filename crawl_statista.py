3a69c3a32ef2e0afc095c82945808a159892f127
cntrl <d.jonda@freenet.de>
2014-09-22 14:24:29 +0200

    minor changes

--- crawl_statista.py
+++ crawl_statista.py
@@ -11,7 +11,6 @@ def mainCrawler(maxPages):
         QuellCode = requests.get(url)
         QuellText = QuellCode.text
         soup = BeautifulSoup(QuellText)
-        # print soup
         for link in soup.findAll('a', {'tabindex': '2'}):
             urlHyperlink = "https://de.statista.com/" + link['href']
             print urlHyperlink
@@ -28,19 +27,5 @@ def getHyperlinks(itemUrl):
     for header in soup.findAll('td'):
         print header
 
-    # for link in soup.findAll('a', {'data-track': 'ATB-AllJobs'}):
-    #     companyName = link.get('href')
-    #     companyName = companyName[47:len(companyName)]
-    #     companyName = companyName.replace('+', ' ')
-    #     companyName = companyName.replace('%26', '&')
-    #     companyName = companyName.replace('%c3%bc', 'ue')
-    #     print companyName
-    #
-    # heading = soup.findAll('h1')
-    # print(heading)
-    #
-
 # Webcrawler starten.
-# Argument 1 gibt den gewünschten Suchbegriff an,
-# Argument 2 die Anzahl an zu crawlenden Suchseiten.
 mainCrawler(1)
